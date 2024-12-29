be8d06c9895f5abd5e62093b77f1ef9af4f5c776
MysticWhiz <ajalif6789@gmail.com>
2024-12-29 13:34:40 +0600

    question

---
+++ question-2.py
@@ -0,0 +1,10 @@
+"""
+Given the string "Python is awesome", use string methods to:
+
+1. Convert the string to all uppercase letters.
+2. Replace the word "awesome" with "fun".
+"""
+#1.ans:
+str = "Python is awesome"
+print(str.upper())
+print(str.replace("awesome","fun"))
